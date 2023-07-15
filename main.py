7c9c0b1500ede191e271cf3e8a1dacff9ffa6846
PilotQuik <malteverhoeff@gmail.com>
2023-07-15 16:16:45 +0200

    finished the main.py rework

--- main.py
+++ main.py
@@ -1,56 +1,71 @@
 import tkinter as tk
-from tkinter import Label
 from tkinter import ttk
+from tkinter import *
+from tkinter import Label
+from events import Event
 from const import *
+from menu import Menu
+from game import Game
 
-class MainFrame(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
 
-        options = {"padx": 10, "pady": 10}
-
-        text = Label(root, text="PLAY", fg="black", font=("Impact", 50))
-        text.place(x=50, y=50)
-
-class App(tk.Tk): # defines Main class inheriting from tk.TK
+class Root(tk.Tk): # defines Main class inheriting from tk.TK
     def __init__(self):
         super().__init__() # initializing tkinter
 
-        self.title("Main")
-        self.geometry(str(MENU_WIDTH) + "x" + str(MENU_HEIGHT))
-        self.wm_iconphoto(False, tk.PhotoImage(file="assets/icon.png"))
+        self.iconbitmap("assets/icon.ico")
 
+        self.frame = Menu(self)
+        self.event = Event(self)
+
+    def findCenter(self, winHeight, winWidth):
+        screenWidth = self.winfo_screenwidth()
+        screenHeight = self.winfo_screenheight()
+
+        centerX = int(screenWidth / 2 - winWidth / 2)
+        centerY = int(screenHeight / 2 - winHeight / 2)
+        return (centerX, centerY)
+
+    def switchFrame(self, newFrame):
+        self.frame.pack_forget()
+        self.frame = newFrame(self)
+"""
+    def display(self, window):
+        if window == "menu":
+            # positions window in the center and creates the menu-canvas
+            self.title("Menu")
+            centerPos = self.findCenter(winHeight=MENU_HEIGHT, winWidth=MENU_WIDTH)
+            self.geometry(f"{MENU_WIDTH}x{MENU_HEIGHT}+{centerPos[0]}+{centerPos[1]}")
+            menuCanvas = Canvas(self, width=MENU_WIDTH, height=MENU_HEIGHT, bg='white')
+            menuCanvas.pack(side=TOP, fill=BOTH, expand=NO)
+            self.currentCanvas = menuCanvas
+
+
+            text = Label(menuCanvas, text="PLAY", fg="black", font=("Impact", 50), background="white")
+            text.place(x=50, y=50)
+        if window == "game":
+            # positions window in the center and creates the game-canvas
+            self.title("GO")
+            centerPos = self.findCenter(winHeight=GAME_HEIGHT, winWidth=GAME_WIDTH)
+            self.geometry(f"{GAME_WIDTH}x{GAME_HEIGHT}+{centerPos[0]}+{centerPos[1]}")
+            gameCanvas = Canvas(self, width=GAME_WIDTH, height=GAME_HEIGHT, bg='white')
+            gameCanvas.pack(side=TOP, fill=BOTH, expand=NO)
+            self.currentCanvas = gameCanvas
+
+            text = Label(gameCanvas, text="go", fg="black", font=("Impact", 50), background="white")
+            text.place(x=50, y=50)
+        if window == "opt":
+            # positions window in the center and creates the options-canvas
+            self.title("Options")
+            centerPos = self.findCenter(winHeight=OPT_HEIGHT, winWidth=OPT_WIDTH)
+            self.geometry(f"{OPT_WIDTH}x{OPT_HEIGHT}+{centerPos[0]}+{centerPos[1]}")
+            optCanvas = Canvas(self, width=OPT_WIDTH, height=OPT_HEIGHT, bg='white')
+            optCanvas.pack(side=TOP, fill=BOTH, expand=NO)
+            self.currentCanvas = optCanvas
+
+            text = Label(optCanvas, text="options", fg="black", font=("Impact", 50), background="white")
+            text.place(x=50, y=50)     
+"""
 if __name__ == "__main__":
-    root = App() # creating instance of Main class
-    frame = MainFrame(root)
+    root = Root() # creating instance of Main class
+    root.bind("<Button-1>", root.event.mouse1) # leftclick-event
     root.mainloop()
-
-
-
-#root = Tk()
-
-# menu window:
-#if CURRENT_WINDOW == "menu":
-#    root.title("MENU")
-#    root.wm_iconphoto(False, tkinter.PhotoImage(file="assets/icon.png"))
-#    menuCanvas = Canvas(root, width=MENU_WIDTH, height=MENU_HEIGHT, bg='yellow')
-#    menuCanvas.pack(side=TOP, fill=BOTH, expand=YES)
-
-# game window
-#if CURRENT_WINDOW == "game":
-#    root.title("GO")
-#    root.wm_iconphoto(False, tkinter.PhotoImage(file="assets/icon.png"))
-#    gameCanvas = Canvas(root, width=MENU_WIDTH, height=MENU_HEIGHT, bg='brown')
-#    gameCanvas.pack(side=TOP, fill=BOTH, expand=YES)
-
-# options window
-#if CURRENT_WINDOW == "opt":
-#    root.title("OPTIONS")
-#    root.wm_iconphoto(False, tkinter.PhotoImage(file="assets/icon.png"))
-#    optCanvas = Canvas(root, width=MENU_WIDTH, height=MENU_HEIGHT, bg='white')
-#    optCanvas.pack(side=TOP, fill=BOTH, expand=YES)
-
-###>----------mainloop----------<###
-
-###>----------mainloop----------<###
-#root.mainloop()
