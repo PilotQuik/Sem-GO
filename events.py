f7400765262a4d8ecdcb764234e0bd3d625e6189
PilotQuik <malteverhoeff@gmail.com>
2023-07-17 17:29:10 +0200

    added a on-hover feature

--- events.py
+++ events.py
@@ -10,20 +10,28 @@ class Event:
     def __init__(self, container):
         self.container = container
     def mouse1(self, event = NONE):
-        print(int(event.x), int(event.y), event.widget, str(event.widget).split(".")[-1])
+        print(int(event.x), int(event.y), event.widget)
         if isinstance(self.container.frame, Menu):
             if str(event.widget).split(".")[-1] == "play-button":
-                self.container.switchFrame(Game)
+                self.container.switchFrame(Game, width=GAME_WIDTH, height=GAME_HEIGHT)
             if str(event.widget).split(".")[-1] == "opt-button":
-                self.container.switchFrame(Opt)
+                self.container.switchFrame(Opt, width=OPT_WIDTH, height=OPT_HEIGHT)
             if str(event.widget).split(".")[-1] == "quit-button":
                 self.container.destroy()
         elif isinstance(self.container.frame, Game):
             if ".!canvas.!label" in str(event.widget):
-                self.container.switchFrame(Opt)
+                self.container.switchFrame(Menu, width=MENU_WIDTH, height=MENU_HEIGHT)
         elif isinstance(self.container.frame, Opt):
             if ".!canvas.!label" in str(event.widget):
-                self.container.switchFrame(Menu)
+                self.container.switchFrame(Menu, width=MENU_WIDTH, height=MENU_HEIGHT)
 
+    def onHoverEnter(self, event=None):
+        if "-button" in str(event.widget).split(".")[-1]:
+            event.widget.config(fg=BUTTON_COL_CLICKED)
 
+    def onHoverLeave(self, event=None):
+        if "-button" in str(event.widget).split(".")[-1]:
+            event.widget.config(fg=BUTTON_COL)
 
+    def config(self, event=None):
+        pass
