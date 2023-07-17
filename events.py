e23e37f2d38d0823f9f6b8550eba0cc812a544b2
PilotQuik <malteverhoeff@gmail.com>
2023-07-17 13:29:56 +0200

    updated the mouse1() method to check for lables clicked

--- events.py
+++ events.py
@@ -1,20 +1,29 @@
+import re
 from tkinter import *
 from const import *
 from main import *
 from game import Game
 from opt import Opt
 
+
 class Event:
     def __init__(self, container):
         self.container = container
     def mouse1(self, event = NONE):
-        print(int(event.x), int(event.y))
+        print(int(event.x), int(event.y), event.widget, str(event.widget).split(".")[-1])
         if isinstance(self.container.frame, Menu):
-            self.container.switchFrame(Game)
+            if str(event.widget).split(".")[-1] == "play-button":
+                self.container.switchFrame(Game)
+            if str(event.widget).split(".")[-1] == "opt-button":
+                self.container.switchFrame(Opt)
+            if str(event.widget).split(".")[-1] == "quit-button":
+                self.container.destroy()
         elif isinstance(self.container.frame, Game):
-            self.container.switchFrame(Opt)
+            if ".!canvas.!label" in str(event.widget):
+                self.container.switchFrame(Opt)
         elif isinstance(self.container.frame, Opt):
-            self.container.switchFrame(Menu)
+            if ".!canvas.!label" in str(event.widget):
+                self.container.switchFrame(Menu)
 
 
 
