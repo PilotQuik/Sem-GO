d0c9feecabcfc8aeace42f62da63e5eee66eeafd
PilotQuik <malteverhoeff@gmail.com>
2023-07-17 13:30:59 +0200

    removed large comment

--- main.py
+++ main.py
@@ -28,43 +28,7 @@ class Root(tk.Tk): # defines Main class inheriting from tk.TK
     def switchFrame(self, newFrame):
         self.frame.pack_forget()
         self.frame = newFrame(self)
-"""
-    def display(self, window):
-        if window == "menu":
-            # positions window in the center and creates the menu-canvas
-            self.title("Menu")
-            centerPos = self.findCenter(winHeight=MENU_HEIGHT, winWidth=MENU_WIDTH)
-            self.geometry(f"{MENU_WIDTH}x{MENU_HEIGHT}+{centerPos[0]}+{centerPos[1]}")
-            menuCanvas = Canvas(self, width=MENU_WIDTH, height=MENU_HEIGHT, bg='white')
-            menuCanvas.pack(side=TOP, fill=BOTH, expand=NO)
-            self.currentCanvas = menuCanvas
 
-
-            text = Label(menuCanvas, text="PLAY", fg="black", font=("Impact", 50), background="white")
-            text.place(x=50, y=50)
-        if window == "game":
-            # positions window in the center and creates the game-canvas
-            self.title("GO")
-            centerPos = self.findCenter(winHeight=GAME_HEIGHT, winWidth=GAME_WIDTH)
-            self.geometry(f"{GAME_WIDTH}x{GAME_HEIGHT}+{centerPos[0]}+{centerPos[1]}")
-            gameCanvas = Canvas(self, width=GAME_WIDTH, height=GAME_HEIGHT, bg='white')
-            gameCanvas.pack(side=TOP, fill=BOTH, expand=NO)
-            self.currentCanvas = gameCanvas
-
-            text = Label(gameCanvas, text="go", fg="black", font=("Impact", 50), background="white")
-            text.place(x=50, y=50)
-        if window == "opt":
-            # positions window in the center and creates the options-canvas
-            self.title("Options")
-            centerPos = self.findCenter(winHeight=OPT_HEIGHT, winWidth=OPT_WIDTH)
-            self.geometry(f"{OPT_WIDTH}x{OPT_HEIGHT}+{centerPos[0]}+{centerPos[1]}")
-            optCanvas = Canvas(self, width=OPT_WIDTH, height=OPT_HEIGHT, bg='white')
-            optCanvas.pack(side=TOP, fill=BOTH, expand=NO)
-            self.currentCanvas = optCanvas
-
-            text = Label(optCanvas, text="options", fg="black", font=("Impact", 50), background="white")
-            text.place(x=50, y=50)     
-"""
 if __name__ == "__main__":
     root = Root() # creating instance of Main class
     root.bind("<Button-1>", root.event.mouse1) # leftclick-event
