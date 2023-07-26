011cc16c055bdeb02c62cb1201ff3faee38c6264
PilotQuik <malteverhoeff@gmail.com>
2023-07-26 16:46:09 +0200

    added feature that allow stone objects to draw themselves

--- src/stone.py
+++ src/stone.py
@@ -1,6 +1,18 @@
 
 
 class Stone:
-    def __init__(self, color, marked=False):
+    def __init__(self,canvas, col, row, color, boardPad, marked=False):
+        self.canvas = canvas
+        self.col = col
+        self.row = row
         self.color = color
         self.marked = marked
+        self.boardPad = boardPad
+
+    def draw(self):
+        self.canvas.create_oval(
+            50 + self.boardPad + self.col * self.boardPad - self.boardPad / 2.5,
+            50 + self.boardPad + self.row * self.boardPad - self.boardPad / 2.5,
+            50 + self.boardPad + self.col * self.boardPad + self.boardPad / 2.5,
+            50 + self.boardPad + self.row * self.boardPad + self.boardPad / 2.5,
+            fill="black" if self.color == "black" else "white")
