a0d310a22d99e0189461bc645dcb30e9c69edaa8
PilotQuik <malteverhoeff@gmail.com>
2023-08-31 17:30:47 +0200

    edit

--- src/stone.py
+++ src/stone.py
@@ -24,4 +24,3 @@ class Stone:
                 padLeft + self.boardPad + self.col * self.boardPad + self.boardPad / 2.5,
                 50 + self.boardPad + self.row * self.boardPad + self.boardPad / 2.5,
                 fill="black" if self.color == "black" else "white")
-            print("hello")
